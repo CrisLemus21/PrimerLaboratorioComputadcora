341c0da8386c6652a6a465aae850af35e20ab17e
CrisLemus21 <87676318+CrisLemus21@users.noreply.github.com>
2021-08-16 13:36:14 -0600

    actualizacion2

--- Computadora/Computadora/Entidades/ClsComputadora.cs
+++ Computadora/Computadora/Entidades/ClsComputadora.cs
@@ -8,5 +8,26 @@ namespace Computadora.Entidades
 {
     class ClsComputadora
     {
+        private String pantalla;
+        private String memoriaRan;
+        private String procesador;
+        private String almacenamiento;
+        private String grafica;
+        private String nombre;
+        private String resolucion;
+        private String sistemaoperativo;
+        private int dimenciones;
+        private String mause;
+
+        public string Pantalla { get => pantalla; set => pantalla = value; }
+        public string MemoriaRan { get => memoriaRan; set => memoriaRan = value; }
+        public string Procesador { get => procesador; set => procesador = value; }
+        public string Almacenamiento { get => almacenamiento; set => almacenamiento = value; }
+        public string Grafica { get => grafica; set => grafica = value; }
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Resolucion { get => resolucion; set => resolucion = value; }
+        public string Sistemaoperativo { get => sistemaoperativo; set => sistemaoperativo = value; }
+        public int Dimenciones { get => dimenciones; set => dimenciones = value; }
+        public string Mause { get => mause; set => mause = value; }
     }
 }
